8fd657c98c18cb0b2c20a56d2b42ad72963222ba
vagrant <alan123art@gmail.com>
2017-08-03 23:08:02 +0000

    Devise Email Config

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -12,6 +12,8 @@ Rails.application.configure do
   #Better Errors Config
   BetterErrors::Middleware.allow_ip! ENV['TRUSTED_IP'] if ENV['TRUSTED_IP']
 
+  #Devise Email Config
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
   # Show full error reports and disable caching.
   config.consider_all_requests_local       = true
   config.action_controller.perform_caching = false
