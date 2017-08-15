7791d540bede4e44c3266ff78c5237ede1a9a64a
vagrant <alan123art@gmail.com>
2017-08-15 00:46:43 +0000

    Aula 17: Config o acesso ao backoffice

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   namespace :site do
   get 'home', to: 'home#index'
   end
+ 
+ get  'backoffice', to: 'backoffice/dashboard#index'
 
   namespace :backoffice do
    get  'dashboard', to: 'dashboard#index'
